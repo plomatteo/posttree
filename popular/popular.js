30d6cce54abead5b028b334b4502991443d65e90
Your Name <you@example.com>
2021-12-22 12:49:27 +0100

    pop

--- popular/popular.js
+++ popular/popular.js
@@ -43,7 +43,7 @@ getPopular(popularity).then(function (data) {
     function printFirst(top) {
         var baseUrl = 'https://image.tmdb.org/t/p/w500';
         document.getElementById('card-poster').src = baseUrl + top.poster_path;
-        document.getElementById('card-title').innerHTML = "più popolare: "+ top.title;
+        document.getElementById('card-title').innerHTML = "Best popular: "+ top.title;
     }
 
     printFirst(e[0]);
