f2e68b834da4849055f81796f18ab798296f3248
Your Name <you@example.com>
2021-12-22 11:49:37 +0100

    nuovo

--- popular/popular.js
+++ popular/popular.js
@@ -1,35 +1,51 @@
-//const key = '0330724af874d28c62a4c079ed817bce';
-//const urlTopRated = 'https://api.themoviedb.org/3/movie/popular?api_key=0330724af874d28c62a4c079ed817bce&language=en-US&page=1';
+const key = '0330724af874d28c62a4c079ed817bce';
+const urlTopRated = 'https://api.themoviedb.org/3/movie/popular?api_key=0330724af874d28c62a4c079ed817bce&language=en-US&page=1';
 
-var url = 'https://api.themoviedb.org/3/movie/popular?api_key=0330724af874d28c62a4c079ed817bce&language=en-US&page=1';
-fetch(url).then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    console.log(data);
-  }).catch(function() {
-    console.log("Booo");
-  });
-  
-  function printList(list) {
-    var str = "";
-
-    list.forEach(v => {
-        str += "<tr>"
-        str += "<td><a>" + v.title + "</a></td>"
-        str += "<td>" + v.popularity + "</td>"
-        str += "</td>"
-    });
-    document.getElementById('popular').innerHTML = str;
+function httpGetPost(url) {
+    return new Promise(function (resolve, reject) {
+        var httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = function () {
+            let post;
+            if (httpReq.readyState == 4) {
+                if (httpReq.status == 200) {
+                    post = JSON.parse(httpReq.responseText);
+                    resolve(post);
+                }
+                else {
+                    reject(new Error(httpReq.statusText))
+                }
+            }
+        }
+        httpReq.open("GET", url, true)
+        httpReq.send();
+    })
 }
 
-printList(e);
+httpGetPost(urlTopRated).then(function (data) {
+    var e = data.results;
+    console.log(e);
 
-function printFirst(top) {
-    var baseUrl = 'https://image.tmdb.org/t/p/w500';
-    document.getElementById('card-poster').src = baseUrl + top.poster_path;
-    document.getElementById('card-title').innerHTML = top.title;
-}
 
-printFirst(e[0]);
+    function printList(list) {
+        var str = "";
+
+        list.forEach(v => {
+            str += "<tr>"
+            str += "<td><a>" + v.title + "</a></td>"
+            str += "<td>" + v.popularity + "</td>"
+            str += "</td>"
+        });
+        document.getElementById('popular').innerHTML = str;
+    }
+
+    printList(e);
+
+    function printFirst(top) {
+        var baseUrl = 'https://image.tmdb.org/t/p/w500';
+        document.getElementById('card-poster').src = baseUrl + top.poster_path;
+        document.getElementById('card-title').innerHTML = top.title;
+    }
+
+    printFirst(e[0]);
 
 });
