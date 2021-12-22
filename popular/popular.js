32a9342fbd2c3325a223fd4091ae3e400f7cc104
Your Name <you@example.com>
2021-12-22 11:25:48 +0100

    nuovo commit

--- popular/popular.js
+++ popular/popular.js
@@ -2,6 +2,11 @@
 //const urlTopRated = 'https://api.themoviedb.org/3/movie/popular?api_key=0330724af874d28c62a4c079ed817bce&language=en-US&page=1';
 
 var url = 'https://api.themoviedb.org/3/movie/popular?api_key=0330724af874d28c62a4c079ed817bce&language=en-US&page=1';
-fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(json))
+fetch(url).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+  }).catch(function() {
+    console.log("Booo");
+  });
+  
