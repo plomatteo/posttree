b59b943039cf32918fe4c76904c24cb8b32ed0ab
Your Name <you@example.com>
2021-12-28 18:38:34 +0100

    stile allineato

--- popular/popular.js
+++ popular/popular.js
@@ -1,51 +1,48 @@
-const key = '0330724af874d28c62a4c079ed817bce';
-const popularity = 'https://api.themoviedb.org/3/movie/popular?api_key=0330724af874d28c62a4c079ed817bce&language=en-US&page=1';
+
+const token = "0330724af874d28c62a4c079ed817bce"
+const urlPopular = `https://api.themoviedb.org/3/movie/now_playing?api_key=${token}&language=en-US&page=1`
+
 
 function getPopular(url) {
     return new Promise(function (resolve, reject) {
         var httpReq = new XMLHttpRequest();
+
+        httpReq.open("GET", url, true);
+
+        httpReq.send();
+
         httpReq.onreadystatechange = function () {
-            let get;
+            var data;
             if (httpReq.readyState == 4) {
                 if (httpReq.status == 200) {
-                    get = JSON.parse(httpReq.responseText);
-                    resolve(get);
+                    data = JSON.parse(httpReq.responseText);
+                    resolve(data);
                 }
-                else {
-                    reject(new Error(httpReq.statusText))
-                }
+                else { reject(new Error(httpReq.statusText)) }
             }
+
         }
-        httpReq.open("GET", url, true)
-        httpReq.send();
     })
 }
 
-getPopular(popularity).then(function (data) {
-    var e = data.results;
-    console.log(e);
+getPopular(urlPopular).then(function (data) {
+    console.log(data);
+    var filmData = "";
 
 
-    function printList(list) {
-        var str = "";
 
-        list.forEach(p => {
-            str += "<tr>"
-            str += "<td><a>" + p.title + "</a></td>"
-            str += "<td>" + p.popularity + "</td>"
-            str += "</td>"
-        });
-        document.getElementById('popular').innerHTML = str;
-    }
 
-    printList(e);
 
-    function printFirst(top) {
-        var baseUrl = 'https://image.tmdb.org/t/p/w500';
-        document.getElementById('card-poster').src = baseUrl + top.poster_path;
-        document.getElementById('card-title').innerHTML = "Best popular: "+ top.title;
-    }
+    data.results.map((film) => {
+        filmData += `<div class='card'>`
+        filmData += `<img src="https://image.tmdb.org/t/p/w400/${film.poster_path}" alt="poster">`
+        filmData += `<div class="hide-pop">Popularity: ${film.popularity}</div>`
+        filmData += `</div>`
+    })
+    document.getElementById("Pop").innerHTML = filmData
+}).catch(function(){ document.write('<h1 style="color:red; position:absolute; top:50%; left:50%; transform:translate(-50%,-50%);">Errore di caricamento della pagina, riprova più tardi.<br>Grazie</h1>')})
 
-    printFirst(e[0]);
 
-});
+
+
+
