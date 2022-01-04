8d87ef65ad8835d57e7bc2ca85cb2edc1b4c8a0a
augustoruggieri <augustoruggieri@gmail.com>
2022-01-04 19:24:38 +0100

    added footer, searchbar, and buttons to navigate between pages

--- toprated/toprated.js
+++ toprated/toprated.js
@@ -1,5 +1,6 @@
 const key = '0330724af874d28c62a4c079ed817bce';
-const urlTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`;
+const apiTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=`;
+const urlRateMovie = `https://api.themoviedb.org/3/movie/{movie_id}/rating?api_key=<<api_key>>`;
 
 function httpGetPost(url) {
     return new Promise(function (resolve, reject) {
@@ -21,51 +22,91 @@ function httpGetPost(url) {
     })
 }
 
-httpGetPost(urlTopRated).then(function (data) {
-    var e = data.results;
-    var topFive = e.slice(0,5);
-    var topTen = e.slice(5,10);
-    var topFifteen = e.slice(10,15);
-    var topTwenty = e.slice(15,20);
-    console.log(e);
+function httpRateMovie(url, rating) {
+    return new Promise(function (resolve, reject) {
+        var httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = function () {
+            var post;
+            if (httpReq.readyState == 4) {
+                if (httpReq.status == 201) {
+                    post = JSON.parse(httpReq.responseText);
+                    resolve(post);
+                }
+                else {
+                    reject(new Error(httpReq.statusText))
+                }
+            }
+        }
+        httpReq.open("POST", url, true)
+        httpReq.send();
+    })
+}
 
+/* CHIAMATA GET TOP RATED PAG 1 */
+
+httpGetPost(apiTopRated).then(function (data) {
     var baseUrl = 'https://image.tmdb.org/t/p/w300';
+    document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) => 
+            `<div class="card">
+                <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
+                <div class="overlay">
+                    <div class="overlaytext">
+                        <button>More info</button>
+                    </div>
+                </div>
+            </div>`
+        ).join('')
+})
 
-    document.getElementById('top-five').innerHTML = topFive.map((movie, i) =>
-        `<div class="card">
-            <img src="${baseUrl + movie.poster_path}"></img>  
-            <div class="overlay">
-                <div class="overlaytext">${i+1}<br>Average rating:${movie.vote_average}</div>
-            </div>
-        </div>`
-    ).join('')
+/* CHIAMATA GET TOP RATED PAG SUCCESSIVE */
 
-    document.getElementById('top-ten').innerHTML = topTen.map((movie, i) =>
-        `<div class="card">
-            <img src="${baseUrl + movie.poster_path}"></img>  
-            <div class="overlay">
-                <div class="overlaytext">${i+1}<br>Average rating:${movie.vote_average}</div>
-            </div>
-        </div>`
-    ).join('')
+var pageNum = 1;
 
-    document.getElementById('top-fifteen').innerHTML = topFifteen.map((movie, i) =>
-        `<div class="card">
-            <img src="${baseUrl + movie.poster_path}"></img>  
-            <div class="overlay">
-                <div class="overlaytext">${i+1}<br>Average rating:${movie.vote_average}</div>
-            </div>
-        </div>`
-    ).join('')
+function callNextPage() {
 
-    document.getElementById('top-twenty').innerHTML = topTwenty.map((movie, i) =>
-        `<div class="card">
-            <img src="${baseUrl + movie.poster_path}"></img>  
-            <div class="overlay">
-                <div class="overlaytext">${i+1}<br>Average rating:${movie.vote_average}</div>
-            </div>
-        </div>`
-    ).join('')
-});
+    pageNum = pageNum + 1;
 
+    httpGetPost(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=${pageNum}`).then(function (data) {
+        console.log(data.results);
+    
+        var baseUrl = 'https://image.tmdb.org/t/p/w300';
+    
+        document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) => 
+            `<div class="card">
+                <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
+                <div class="overlay">
+                    <div class="overlaytext">
+                        <button>More info</button>
+                    </div>
+                </div>
+            </div>`
+        ).join('')
+    })}
+
+function callPreviousPage() {
+
+    if(pageNum <= 1) {
+        pageNum = 1;
+    } else {
+        pageNum = pageNum - 1;
+    }
+    
+
+    httpGetPost(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=${pageNum}`).then(function (data) {
+        console.log(data.results);
+    
+        var baseUrl = 'https://image.tmdb.org/t/p/w300';
+    
+        document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) => 
+            `<div class="card">
+                <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
+                <div class="overlay">
+                    <div class="overlaytext">
+                        <button>More info</button>
+                    </div>
+                </div>
+            </div>`
+        ).join('')
+    })
+}
 
