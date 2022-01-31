09a940341ae2c6dce13bdb5dc50d230a6038622d
augustoruggieri <augustoruggieri@gmail.com>
2022-01-31 13:31:04 +0100

    modal window

--- toprated/toprated.js
+++ toprated/toprated.js
@@ -1,6 +1,7 @@
 const key = '0330724af874d28c62a4c079ed817bce';
 const apiTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=`;
 const urlRateMovie = `https://api.themoviedb.org/3/movie/{movie_id}/rating?api_key=<<api_key>>`;
+const urlAddToWatchlist = `https://api.themoviedb.org/3/account/{account_id}/watchlist?api_key=${key}`;
 
 function httpGetPost(url) {
     return new Promise(function (resolve, reject) {
@@ -22,7 +23,29 @@ function httpGetPost(url) {
     })
 }
 
+
+
 function httpRateMovie(url, rating) {
+    return new Promise(function (resolve, reject) {
+        var httpReq = new XMLHttpRequest();
+        httpReq.onreadystatechange = function () {
+            var post;
+            if (httpReq.readyState == 4) {
+                if (httpReq.status == 201) {
+                    post = JSON.parse(httpReq.responseText);
+                    resolve(post);
+                }
+                else {
+                    reject(new Error(httpReq.statusText))
+                }
+            }
+        }
+        httpReq.open("POST", url, true)
+        httpReq.send();
+    })
+}
+
+function httpAddToWatchlist(url, id) {
     return new Promise(function (resolve, reject) {
         var httpReq = new XMLHttpRequest();
         httpReq.onreadystatechange = function () {
@@ -45,17 +68,19 @@ function httpRateMovie(url, rating) {
 /* CHIAMATA GET TOP RATED PAG 1 */
 
 httpGetPost(apiTopRated).then(function (data) {
-    var baseUrl = 'https://image.tmdb.org/t/p/w300';
-    document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) => 
-            `<div class="card">
+    var baseUrl = 'https://image.tmdb.org/t/p/w200';
+    document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) =>
+        `<div class="card" onclick="showModal(${i})">
                 <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
-                <div class="overlay">
-                    <div class="overlaytext">
-                        <button>More info</button>
+                <div id="${i}" class="modal">
+                    <div class="modal-content">
+                        <span class="close" onclick="(() => document.getElementById(${i}).style.color="red")>&times;</span>
+                        <p>${movie.overview}</p>
                     </div>
-                </div>
+
+            </div>
             </div>`
-        ).join('')
+    ).join('')
 })
 
 /* CHIAMATA GET TOP RATED PAG SUCCESSIVE */
@@ -68,41 +93,15 @@ function callNextPage() {
 
     httpGetPost(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=${pageNum}`).then(function (data) {
         console.log(data.results);
-    
-        var baseUrl = 'https://image.tmdb.org/t/p/w300';
-    
-        document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) => 
+
+        var baseUrl = 'https://image.tmdb.org/t/p/w200';
+
+        document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) =>
             `<div class="card">
                 <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
                 <div class="overlay">
                     <div class="overlaytext">
-                        <button>More info</button>
-                    </div>
-                </div>
-            </div>`
-        ).join('')
-    })}
-
-function callPreviousPage() {
-
-    if(pageNum <= 1) {
-        pageNum = 1;
-    } else {
-        pageNum = pageNum - 1;
-    }
-    
-
-    httpGetPost(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=${pageNum}`).then(function (data) {
-        console.log(data.results);
-    
-        var baseUrl = 'https://image.tmdb.org/t/p/w300';
-    
-        document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) => 
-            `<div class="card">
-                <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
-                <div class="overlay">
-                    <div class="overlaytext">
-                        <button>More info</button>
+                        
                     </div>
                 </div>
             </div>`
@@ -110,3 +109,47 @@ function callPreviousPage() {
     })
 }
 
+function callPreviousPage() {
+
+    if (pageNum <= 1) {
+        pageNum = 1;
+    } else {
+        pageNum = pageNum - 1;
+    }
+
+
+    httpGetPost(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=${pageNum}`).then(function (data) {
+        console.log(data.results);
+
+        var baseUrl = 'https://image.tmdb.org/t/p/w200';
+
+        document.getElementById('page-wrap').innerHTML = data.results.map((movie, i) =>
+            `<div class="card">
+                <img class="poster" src="${baseUrl + movie.poster_path}" alt="poster"></img>  
+                <div class="overlay">
+                    <div class="overlaytext">
+                        
+                    </div>
+                </div>
+            </div>`
+        ).join('')
+    })
+}
+
+function movieInfoPage() {
+
+}
+
+function showModal(id) {
+    var modal = document.getElementById(id);
+
+    // When the user clicks the card, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
